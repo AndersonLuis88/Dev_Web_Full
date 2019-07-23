928d8e88bb04632c4a7e4a2c703c3de60f1c5dc2
Anderson <andersonluis1486@gmail.com>
2019-07-23 02:08:44 -0300

    Terminada configuração link dinâmico e layout padrão ou personalizado

--- miniFramework/App/Controllers/indexController.php
+++ miniFramework/App/Controllers/indexController.php
@@ -2,15 +2,22 @@
 
 namespace App\Controllers;
 
-class IndexController {
+use MF\Controller\Action;
+
+class IndexController extends Action {
+	
 
 	public function index() {
-		echo 'Chegamos ao IndexController e disparamos a action index';
+		$this->view->dados = array('Code 1','Code 2','Code 3');
+		$this->render('index', 'layout');
 	}
 
 	public function sobreNos() {
-		echo 'Chegamos ao IndexController e disparamos a action sobreNos';
+		$this->view->dados = array('Code 4','Code 5','Code 6');
+		$this->render('sobreNos', 'layout');
 	}
+
+	
 }
 
 
--- miniFramework/App/Route.php
+++ miniFramework/App/Route.php
@@ -1,22 +1,12 @@
 <?php
 namespace App;
 
-class Route{
-	private $routes;
+use MF\Init\Bootstrap;
 
-	public function __construct(){
-		$this->initRoutes();
-		$this->run($this->getUrl());
-	}
+class Route extends Bootstrap {
+	
 
-	public function getRoutes(){
-		return $this->routes;
-	}
-	public function setRoutes(array $routes){
-		$this->routes = $routes;
-	}
-
-	public function initRoutes(){
+	protected function initRoutes(){
 		$routes['home'] = array(
 			'route' => '/',
 			'controller' => 'indexController',
@@ -33,24 +23,6 @@ class Route{
 
 	}
 
-	public function run($url){
-		foreach ($this->getRoutes() as $key => $route) { 			
-
-			if($url == $route['route']){
-				$class = "App\\Controllers\\".ucfirst($route['controller']);
-
-				$controller = new $class;
-
-				$action = $route['action'];
-
-				$controller->$action();
-			}
-		}
-	}
-
-	public function getUrl(){
-		return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-	}
 
 }
 
